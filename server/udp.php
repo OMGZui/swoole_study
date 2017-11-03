5ba1d06c5fbfb9f87853dc95e630d9c3fdaeedce
omgzui <862275679@qq.com>
2017-11-03 23:33:15 +0800

    简单UDP服务器

---
+++ server/udp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shengj
+ * Date: 2017/11/3
+ * Time: 23:32
+ */
+
+//创建Server对象，监听 127.0.0.1:9502端口，类型为SWOOLE_SOCK_UDP
+$server = new swoole_server("104.223.3.138", 9501, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+
+//监听数据接收事件
+$server->on('Packet', function ($server, $data, $clientInfo) {
+    $server->sendto($clientInfo['address'], $clientInfo['port'], "Server ".$data);
+    var_dump($clientInfo);
+});
+
+//启动服务器
+$server->start();
