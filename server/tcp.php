075e1b1731fded1a948fbc9c81a8f4b1130ca279
omgzui <862275679@qq.com>
2017-11-04 09:00:19 +0800

    异步任务

--- server/tcp.php
+++ server/tcp.php
@@ -11,14 +11,30 @@
 //创建Server对象，监听 127.0.0.1:9501端口
 $server = new swoole_server("104.223.3.138", 9501);
 
+//设置异步任务的工作进程数量
+$server->set(array('task_worker_num' => 4));
+
 //监听连接进入事件
 $server->on('connect', function ($server, $fd) {
     echo "Client: Connect.\n";
 });
 
-//监听数据接收事件
-$server->on('receive', function ($server, $fd, $from_id, $data) {
-    $server->send($fd, "Server: ".$data);
+$server->on('receive', function($server, $fd, $from_id, $data) {
+    //投递异步任务
+    $task_id = $server->task($data);
+    echo "Dispath AsyncTask: id=$task_id\n";
+});
+
+//处理异步任务
+$server->on('task', function ($server, $task_id, $from_id, $data) {
+    echo "New AsyncTask[id=$task_id]".PHP_EOL;
+    //返回任务执行的结果
+    $server->finish("$data -> OK");
+});
+
+//处理异步任务的结果
+$server->on('finish', function ($server, $task_id, $data) {
+    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
 });
 
 //监听连接关闭事件
